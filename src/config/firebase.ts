b65cb7814d7a98b28dc494d65c4c8dbbe84b2b67
Pranjul Jain <92634779+Pranjul-Jain@users.noreply.github.com>
2026-01-25 16:59:14 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrigjFmhUrakXcVxTz8pa1nnQuNLTD9qk",
-  authDomain: "qwiklabs-gcp-04-8d481e2f-df623.firebaseapp.com",
-  projectId: "qwiklabs-gcp-04-8d481e2f-df623",
-  storageBucket: "qwiklabs-gcp-04-8d481e2f-df623.firebasestorage.app",
-  messagingSenderId: "967414883201",
-  appId: "1:967414883201:web:e9c20f9e75219dbe02be80"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
